76eae3ed70b6fdbf60c4d9cc89768501267c1f96
TommasoGalistu <tommasogalistu92@gmail.com>
2024-05-05 10:34:53 +0200

    creazione array review

---
+++ js/javascript.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+
+}
+
+let review = [{
+    immagine: "../img/uomo1.webp",
+    nome: 'tommaso',
+    cognome: 'galistu',
+    professione: 'militare',
+    recensione: 'Ho avuto il piacere di lavorare con questo sviluppatore e sono rimasto colpito dalla velocità con cui è stato in grado di ottimizzare il nostro sito web. Non solo ha migliorato i tempi di caricamento, ma ha anche assicurato che l’esperienza utente rimanesse fluida e intuitiva. È chiaro che la performance è una priorità assoluta per lui.'
+},  
+{
+    immagine: "../img/uomo2.webp",
+    nome: 'francesco',
+    cognome: 'docci',
+    professione: 'studente',
+    recensione: "La capacità di scrivere codice pulito e manutenibile è una rarità, ma questo sviluppatore ha dimostrato di possederla. La struttura del sito è logica e ben organizzata, rendendo facile per noi fare aggiornamenti e manutenzione senza intoppi. Il suo lavoro è un esempio eccellente di programmazione di alta qualità."
+}
+{
+    immagine: "../img/donna1.webp",
+    nome: 'barbara',
+    cognome: 'papini',
+    professione: 'psicoterapeuta',
+    recensione: "Sono rimasto estremamente soddisfatto del livello di dettaglio che questo sviluppatore ha dedicato al nostro progetto. Ogni aspetto del sito è stato curato con precisione, dalle animazioni sottili ai gradienti di colore, tutto contribuisce a un’esperienza utente superiore."
+}
+{
+    immagine: "../img/donna2.webp",
+    nome: 'gaia',
+    cognome: 'galistu',
+    professione: 'ostetrica',
+    recensione: "Questo programmatore non si limita a seguire le specifiche; va oltre. Ha introdotto soluzioni innovative che non solo hanno risolto i problemi esistenti, ma hanno anche aperto la strada a nuove possibilità per il nostro sito web. La sua proattività nel suggerire miglioramenti ha avuto un impatto significativo sulla nostra presenza online."
+}
+
+]
