f6ef2e25d5d5f5da3b02673e427d0edf8cbea409
TommasoGalistu <tommasogalistu92@gmail.com>
2024-05-05 11:07:43 +0200

    creazione funzione per inserimento recensione al caricamento della pagina

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,7 @@
+//collegamenti con html
 let immagine = document.querySelector('img')
-let nomeCompleto = document.querySelector('#nome')
+let nome = document.querySelector('#nome')
+let cognome = document.querySelector('#cognome')
 let professione = document.querySelector('#lavoro')
 let recensione = document.querySelector('#review')
 let buttonAvanti = document.querySelector('#avanti')
@@ -7,16 +9,23 @@ let buttonIndietro = document.querySelector('#indietro')
 let buttonRandom = document.querySelector('#randomButton')
 
 
-window.onload = () => {
 
+//al caricamento della pagina
+window.onload = () => {
+    personReview()//callback
 }
 
+//variabile che indica la persona corrente dell'array
+let persona = 0 ;
+
+
+//array di persone che hanno effettuato la recensione
 let arrReview = [
 {
-    immagine: "../img/uomo2.webp",
-    nome: 'francesco',
-    cognome: 'docci',
-    professione: 'studente',
+    immagine: "../img/uomo2.jpeg",
+    nome: 'Francesco',
+    cognome: 'Docci',
+    professione: 'Studente',
     recensione: "La capacità di scrivere codice pulito e manutenibile è una rarità, ma questo sviluppatore ha dimostrato di possederla. La struttura del sito è logica e ben organizzata, rendendo facile per noi fare aggiornamenti e manutenzione senza intoppi. Il suo lavoro è un esempio eccellente di programmazione di alta qualità."
 },
 {
@@ -32,7 +41,7 @@ let arrReview = [
     cognome: 'papini',
     professione: 'psicoterapeuta',
     recensione: "Sono rimasto estremamente soddisfatto del livello di dettaglio che questo sviluppatore ha dedicato al nostro progetto. Ogni aspetto del sito è stato curato con precisione, dalle animazioni sottili ai gradienti di colore, tutto contribuisce a un’esperienza utente superiore."
-}
+},
 {
     immagine: "../img/donna2.webp",
     nome: 'gaia',
@@ -42,3 +51,14 @@ let arrReview = [
 }
 
 ]
+
+//funzione per l'inserimento della persona
+function personReview(){
+    let item = arrReview[persona]
+    immagine.src = item.immagine
+    nome.textContent = item.nome
+    cognome.textContent = item.cognome
+    professione.textContent = item.professione
+    recensione.textContent = item.recensione
+}
+
